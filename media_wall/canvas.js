b934a993e60713289debc284c132dd5d2266b3dc
Nayeon Seo <nayeon@thegrap.com>
2021-04-29 19:02:06 +0900

    Add random function

--- media_wall/canvas.js
+++ media_wall/canvas.js
@@ -29,10 +29,36 @@ const c = canvas.getContext('2d');
 //   c.stroke();
 // }
 
-let x = 200;
-let y = 200;
-let dx = 5; // 속도
-let dy = 5; // 속도
+// ******** 고정값 ******** //
+// let x = 200;
+// let y = 200;
+// let dx = 5; // 속도
+// let dy = 5;
+// let radius = 30;
+
+// const animate = () => {
+//   requestAnimationFrame(animate);
+//   c.clearRect(0, 0, innerWidth, innerHeight);
+
+//   c.beginPath();
+//   c.arc(x, y, radius, 0, Math.PI * 2, false);
+//   c.strokeStyle = 'blue';
+//   c.stroke();
+
+//   if (x + radius > innerWidth || x - radius < 0) {
+//     dx = -dx;
+//   }
+//   if (y + radius > innerHeight || y - radius < 0) {
+//     dy = -dy;
+//   }
+//   x += dx;
+//   y += dy;
+// };
+
+let x = Math.random() * innerWidth;
+let y = Math.random() * innerHeight;
+let dx = (Math.random() - 0.5) * 8;
+let dy = (Math.random() - 0.5) * 8;
 let radius = 30;
 
 const animate = () => {
