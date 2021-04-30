5a2f25d7a10cc2ca3623fc9374cbff3d35c60c8e
Nayeon Seo <nayeon@thegrap.com>
2021-04-30 19:10:29 +0900

    Add image

--- media_wall/canvas.js
+++ media_wall/canvas.js
@@ -1,83 +1,74 @@
+// 캔버스 사이즈 설정
 const canvas = document.querySelector('canvas');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const c = canvas.getContext('2d');
 
-// Arc / Circle
-// context.arc(x,y,r,sAngle,eAngle,counterclockwise);
-// c.beginPath();
-// c.arc(500, 500, 100, 0, Math.PI * 2, false);
-// c.strokeStyle = 'black';
-// c.stroke();
+// 컨텍스트 걸어주고
+let ctx = canvas.getContext('2d');
 
-// const array = ['tomato', 'pink', 'yellow', 'skyblue', 'gray', 'white'];
+// 이미지 지정
+// const kimchi = new Image();
+// const mulKimchi = new Image();
+// const cucumberKimchi = new Image();
 
-// function getRandomIntInclusive(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
-// }
+let imgArray = [];
 
-// for (let i = 0; i < 100; i++) {
-//   const x = Math.random() * window.innerWidth;
-//   const y = Math.random() * window.innerHeight;
+// 5) 랜덤하게 움직이는 원 생성하기
+function Kimchi(src, x, y, dx, dy) {
+  this.src = src;
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
 
-//   c.beginPath();
-//   c.arc(x, y, 50, 0, Math.PI * 2, false);
-//   c.strokeStyle = array[getRandomIntInclusive(0, array.length)];
-//   c.stroke();
-// }
+  this.draw = () => {
+    let img = new Image();
+    img.onload = function () {
+      ctx.drawImage(img, this.x, this.y);
+    };
+  };
 
-// ******** 고정값 ******** //
-// let x = 200;
-// let y = 200;
-// let dx = 5; // 속도
-// let dy = 5;
-// let radius = 30;
+  this.update = () => {
+    if (this.x > innerWidth || this.x < 0) {
+      this.dx = -this.dx;
+    }
+    this.x += this.dx;
+    this.draw();
+  };
+}
 
-// const animate = () => {
-//   requestAnimationFrame(animate);
-//   c.clearRect(0, 0, innerWidth, innerHeight);
+for (let i = 0; i < 100; i++) {
+  let x = Math.random() * innerWidth;
+  let y = Math.random() * innerHeight;
+  let dx = (Math.random() - 0.5) * 8; // canvas에서 그려지는 x축 위치
+  let dy = 1;
+  let src = './images/kimchi.png';
+  imgArray.push(new Kimchi(src, x, y, dx, dy));
+}
 
-//   c.beginPath();
-//   c.arc(x, y, radius, 0, Math.PI * 2, false);
-//   c.strokeStyle = 'blue';
-//   c.stroke();
-
-//   if (x + radius > innerWidth || x - radius < 0) {
-//     dx = -dx;
-//   }
-//   if (y + radius > innerHeight || y - radius < 0) {
-//     dy = -dy;
-//   }
-//   x += dx;
-//   y += dy;
-// };
-
-let x = Math.random() * innerWidth;
-let y = Math.random() * innerHeight;
-let dx = (Math.random() - 0.5) * 8;
-let dy = (Math.random() - 0.5) * 8;
-let radius = 30;
+console.log(imgArray);
 
 const animate = () => {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
 
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = 'blue';
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
+  for (let i = 0; i < imgArray.length; i++) {
+    imgArray[i].update();
   }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-  x += dx;
-  y += dy;
 };
-
 animate();
+// }
+
+// function printImages_a(img, x, y) {
+//   function animate() {
+//     requestAnimationFrame(animate);
+//     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+//     ctx.drawImage(img, x, y);
+
+//     x += 1;
+//     if (x > ctx.canvas.width) {
+//       x = -10;
+//     }
+//   }
+//   // animate();
+// }
--- resize/canvas.js
+++ resize/canvas.js
@@ -1,43 +1,97 @@
 const canvas = document.querySelector('canvas');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const c = canvas.getContext('2d');
 
-// Fill
-c.fillStyle = 'rgba(255,0,0,0.5)';
-c.fillRect(100, 100, 100, 100);
+// 1) Fill
+// c.fillStyle = 'rgba(255,0,0,0.5)';
+// c.fillRect(100, 100, 100, 100);
 
-c.fillStyle = 'rgba(0,0,255,0.5)';
-c.fillRect(250, 250, 50, 50);
+// c.fillStyle = 'rgba(0,0,255,0.5)';
+// c.fillRect(250, 250, 50, 50);
 
-c.fillRect(500, 500, 100, 100);
-c.fillRect(800, 550, 100, 100);
-console.log(canvas);
+// c.fillRect(500, 500, 100, 100);
+// c.fillRect(800, 550, 100, 100);
+// console.log(canvas);
 
-// Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = 'tomato';
-c.stroke();
+// 2) Line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = 'tomato';
+// c.stroke();
 
-// Arc / Circle
+// 3) Arc / Circle
 // context.arc(x,y,r,sAngle,eAngle,counterclockwise);
-c.beginPath();
-c.arc(500, 500, 100, 0, Math.PI * 2, false);
-c.strokeStyle = 'black';
-c.stroke();
+// c.beginPath();
+// c.arc(100, 100, 10, 0, Math.PI * 2, false);
+// c.strokeStyle = 'black';
+// c.stroke();
+
+// 4) 랜덤 컬러로 나타나는 원 생성하기
+// for (let i = 0; i < 100; i++) {
+//   const x = Math.random() * window.innerWidth;
+//   const y = Math.random() * window.innerHeight;
+//   const colors = ['red', 'pink', 'green', 'skyblue', 'gray', 'white'];
+
+//   let color = Math.floor(Math.random() * colors.length);
+//   c.beginPath();
+//   c.arc(x, y, 50, 0, Math.PI * 2, false);
+//   c.strokeStyle = colors[color];
+//   c.stroke();
+// }
+
+// 5) 랜덤하게 움직이는 원 생성하기
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+
+  this.draw = () => {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+  };
+
+  this.update = () => {
+    if (this.x + radius > innerWidth || this.x - radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + radius > innerHeight || this.y - radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  };
+}
+
+let circleArray = [];
 
 for (let i = 0; i < 100; i++) {
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
-  const colors = ['red', 'pink', 'green', 'skyblue', 'gray', 'white'];
-  let color = (Math.random() * colors.length).floor;
-  c.beginPath();
-  c.arc(x, y, 100, 0, Math.PI * 2, false);
-  c.strokeStyle = colors[color];
-  c.stroke();
+  let x = Math.random() * innerWidth;
+  let y = Math.random() * innerHeight;
+  let dx = (Math.random() - 0.5) * 8; // canvas에서 그려지는 x축 위치
+  let dy = (Math.random() - 0.5) * 8; // canvas에서 그려지는 y축 위치
+  let radius = 30;
+
+  circleArray.push(new Circle(x, y, dx, dy, radius));
 }
+
+console.log(circleArray);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+};
+
+animate();
